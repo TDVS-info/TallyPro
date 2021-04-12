522f9e9bbc5a18974b0c38d3f3bd465242de563e
TDVS-info <80644105+TDVS-info@users.noreply.github.com>
2021-04-12 18:33:16 -0400

    Add files via upload

---
+++ code/MAIN_081/tallylight.h
@@ -0,0 +1,53 @@
+/*
+  tallylight.h - static strings.
+  Created by Ted Dillenkofer, January 22, 2021.
+*/
+
+#ifndef _TALLYLIGHT_H    
+#define _TALLYLIGHT_H 
+
+const String HTMLheader = "<!DOCTYPE HTML><html><head><meta content=\"text/html; charset=ISO-8859-1\"http-equiv=\"content-type\"><title>Lap Counter</title><style>\"body { background-color: #808080; font-family: Arial, Helvetica, Sans-Serif; Color: #000000; }\"</style></head><body>";
+
+const char *ESPssid = "TallyLight";
+const char *ESPpassword = "TallyLight";
+IPAddress ap_local_IP(192,168,4,1);
+IPAddress ap_gateway(192,168,4,254);
+IPAddress ap_subnet(255,255,255,0);
+
+#define OBS   0
+#define ATEM  1
+#define VMIX  2
+
+#define both  0
+#define front 1
+#define back  2
+
+int offset_cflag = 0;
+int offset_ssid = 1;
+int offset_pswd = 33;
+int offset_SwitchType = 65;
+int offset_SwitchIP = 67;
+int offset_SwitchPort = 99;
+int offset_SwitchPswd = 131;
+int offset_Camera_ID = 163;
+int offset_LED_pos = 195;
+int offset_sip = 227;
+int offset_gateway = 259;
+int offset_subnet = 291;
+
+String   wifi_ssid;
+String   wifi_pswd;
+String   wifi_sip;
+String   wifi_gateway;
+String   wifi_subnet;
+uint16_t Switch_Type;
+String	 Switch_IP;
+String   Switch_Port;
+String   Switch_Pswd;
+String   Camera_ID;
+uint16_t camera_num;
+uint16_t LED_pos;
+
+String   CONFIGURED;
+
+#endif // _TALLYLIGHT_H
